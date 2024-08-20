43e2b47fdab13c68fd9e4256f170e7a3b09f1e06
Sebbybobbler <seb_davies@hotmail.co.uk>
2024-08-20 14:57:46 +0100

    Define type for incoming case data via API, also check it works.

--- src/CasesView.tsx
+++ src/CasesView.tsx
@@ -1,5 +1,36 @@
+import { makeApiRequest } from "./services/apiService";
+
+interface CaseObject {
+    totalCases: number;
+    totalPages: number;
+    currentPage: number;
+    limit: number;
+    data: [
+        {
+            id: string;
+            creation_date: string;
+            case_key: string;
+            patient: string;
+            status: string;
+            specialty: string;
+            owner: string;
+            reporting_specialist: string;
+            species: string;
+            body_areas: string;
+            turnaround: string;
+            reported_date: string;
+            image_url: string;
+        }
+    ];
+}
+
 function CasesView() {
-    return <div></div>;
+    const allCases: Promise<CaseObject> = makeApiRequest("");
+    allCases.then((result) => {
+        console.log(result.data);
+    });
+
+    return <></>;
 }
 
 export default CasesView;
