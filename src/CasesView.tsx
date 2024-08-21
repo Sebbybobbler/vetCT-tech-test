648c0669ecbc476fd208422d4b1fbf01a5e24d72
Seb Davies <seb_davies@hotmail.co.uk>
2024-08-21 22:59:22 +0100

    add search bar. Still need to filter array of return rows.

--- src/CasesView.tsx
+++ src/CasesView.tsx
@@ -4,72 +4,101 @@ import Row from "./components/Row/Row";
 import "./CasesView.css";
 
 export interface CaseObject {
-    totalCases: number;
-    totalPages: number;
-    currentPage: number;
-    limit: number;
-    data: [
-        {
-            id: string;
-            creation_date: string;
-            case_key: string;
-            patient: string;
-            status: string;
-            specialty: string;
-            owner: string;
-            reporting_specialist: string;
-            species: string;
-            body_areas: string;
-            turnaround: string;
-            reported_date: string;
-            image_url: string;
-        }
-    ];
+  totalCases: number;
+  totalPages: number;
+  currentPage: number;
+  limit: number;
+  data: [
+    {
+      id: string;
+      creation_date: string;
+      case_key: string;
+      patient: string;
+      status: string;
+      specialty: string;
+      owner: string;
+      reporting_specialist: string;
+      species: string;
+      body_areas: string;
+      turnaround: string;
+      reported_date: string;
+      image_url: string;
+    }
+  ];
 }
 
 function CasesView() {
-    // eslint-disable-next-line @typescript-eslint/no-explicit-any
-    const [response, setResponse] = useState<any>(null);
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  const [response, setResponse] = useState<any>(null);
+  const [search, setSearch] = useState<string>("");
 
-    useEffect(() => {
-        async function initialRequest(): Promise<CaseObject> {
-            const allCases: Promise<CaseObject> = await makeApiRequest("");
-            setResponse((await allCases).data);
-            return allCases;
-        }
-        initialRequest();
-    }, []);
-    if (!response) {
-        return (
-            <>
-                <div>
-                    <p>Loading...</p>
-                </div>
-            </>
-        );
-    } else {
-        return (
-            <>
-                <table className="casesTable">
-                    <tbody>
-                        <tr>
-                            <th>Case Key</th>
-                            <th>Name</th>
-                            <th>Owner</th>
-                            <th>Specialty</th>
-                            <th>Date</th>
-                            <th></th>
-                        </tr>
-                        {response.map(
-                            (element: CaseObject["data"][0], index: number) => {
-                                return <Row key={index} row={element} />;
-                            }
-                        )}
-                    </tbody>
-                </table>
-            </>
-        );
+  useEffect(() => {
+    async function initialRequest(): Promise<CaseObject> {
+      const allCases: Promise<CaseObject> = await makeApiRequest("");
+      setResponse((await allCases).data);
+      return allCases;
     }
+    initialRequest();
+  }, []);
+
+  //   async function searchFilter(response, search) {
+  //     const filteredArray = (await response).filter(async (element) => {
+  //       await element.patient.toLowerCase().includes(search.toLowerCase());
+  //     });
+  //     return await filteredArray;
+  //   }
+  //   const thing = searchFilter(response, search);
+  //   console.log(thing);
+  //   //   console.log(response);
+  //   //   async function searchFilter(response) {
+  //   //     await response.then((response) => {
+  //   //       const filteredArray = response.filter((element) => {
+  //   //         element.patient.toLowerCase().includes(search.toLowerCase());
+  //   //         console.log(filteredArray);
+  //   //       });
+  //   //     });
+  //   //   }
+  //   //   searchFilter(response);
+
+  function handleChange(e: Event) {
+    const value: string = e.target.value;
+    setSearch(value);
+  }
+  if (!response) {
+    return (
+      <>
+        <div>
+          <p>Loading...</p>
+        </div>
+      </>
+    );
+  } else {
+    return (
+      <>
+        <input
+          type="text"
+          placeholder="search"
+          onChange={handleChange}
+          value={search}
+        />
+        <table className="casesTable">
+          <tbody>
+            <tr>
+              <th>Case Key</th>
+              <th>Name</th>
+              <th>Owner</th>
+              <th>Specialty</th>
+              <th>Date</th>
+              <th></th>
+            </tr>
+            {response.map((element: CaseObject["data"][0], index: number) => {
+              return <Row key={index} row={element} />;
+            })}
+          </tbody>
+        </table>
+      </>
+    );
+  }
 }
 
 export default CasesView;
