4c21a4a4d5e322984932c3f9f980972d2f61e0be
Seb Davies <seb_davies@hotmail.co.uk>
2024-08-25 23:22:32 +0100

    add styling and make page response for all-cases view

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -5,12 +5,12 @@ function Header() {
     <header>
       <img
         alt="Vet.CT Logo"
-        className="headerLogo"
+        className="headerLogo headerItem"
         src="https://euw2-prod-vetct-public-assets.s3.eu-west-2.amazonaws.com/vetct-logos/VETCT-logo-cropped-burgundy.png"
       />
       <img
         alt="Profile Image"
-        className="headerProfile"
+        className="headerProfile headerItem"
         src="https://upload.wikimedia.org/wikipedia/commons/thumb/a/af/Default_avatar_profile.jpg/512px-Default_avatar_profile.jpg?20231202140256"
       />
     </header>
