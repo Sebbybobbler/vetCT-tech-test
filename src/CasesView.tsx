037217e42d24f86dfb6894d5e339c69e40d5e53f
Seb Davies <seb_davies@hotmail.co.uk>
2024-08-26 19:01:21 +0100

    code comments

--- src/CasesView.tsx
+++ src/CasesView.tsx
@@ -136,6 +136,7 @@ function CasesView() {
       setCurrentPage(nextPage);
     }
   }
+
   // Function for fetching prev page, if user is searching it will only update the currentPage state and the data displayed is then handled by the searchFilter function.
   async function prevPage() {
     const prevPage: number = currentPage - 1;
